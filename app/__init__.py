754bb49752b8e8ab874e3ae4528901d59858a3fd
Joel <joelvieirasouza@gmail.com>
2024-01-30 15:20:37 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://hotel631_feedback:8mVYt{DFPV,b@br888.hostgator.com.br/hotel631_feedback'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://hotel631_adm_data_user:8mVYt{DFPV,b@br888.hostgator.com.br/hotel631_adm_data'
 
 app.config['SECRET_KEY'] = 'super-secret-key'
 
