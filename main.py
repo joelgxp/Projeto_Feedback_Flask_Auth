dff323c6d2e2d5cbed890292d083dd42e73452e1
Marianna Rodrigues <mxriannar@gmail.com>
2024-02-02 15:04:46 -0300

    Ajustando front-end.

--- main.py
+++ main.py
@@ -63,20 +63,31 @@ def load_user(user_id):
 
 # END LOGIN
 
-# Lider
-@app.route('/lider', methods=['GET', 'POST'])
+# Employees
+@app.route('/employees', methods=['GET', 'POST'])
 @login_required
-def lider():
+def employees():
     if request.method == 'GET':
-        lideres = Users.query.all()
-        print(lideres)
-        return render_template('lider.html', lideres=lideres)
+        employees = Users.query.all()
+        print(employees)
+        return render_template('employees.html', employees=employees)
     
     elif request.method == 'POST':
         
-        return render_template('lider.html')
+        return render_template('employees.html')
     
-   
-
-
+    
+@app.route('/new-employee', methods=['GET', 'POST'])
+@login_required
+def new_employee():
+    if request.method == 'GET':
+        employees = Users.query.all()
+        print(employees)
+        return render_template('new-employee.html', employees=employees)
+    
+    elif request.method == 'POST':
+        
+        return render_template('employees.html')
+    
+    
 app.run(debug=True)
